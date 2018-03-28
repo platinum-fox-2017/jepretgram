617d907e2689f9eac02f56e70d1a2600525d2b7c
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-03-28 15:40:18 +0700

    Create virtual column for likes

--- server/models/post.js
+++ server/models/post.js
@@ -4,7 +4,16 @@ const Schema = mongoose.Schema
 const postSchema = new Schema({
   photo:  String,
   caption: String,
-  user: { type: Schema.Types.ObjectId, ref: 'User' }
-})
+  user: { type: Schema.Types.ObjectId, ref: 'User' }},
+  { toJSON: { virtuals: true } }
+)
+postSchema.virtual('likes', {
+  ref: 'LikePost', // The model to use
+  localField: '_id', // Find people where `localField`
+  foreignField: 'post', // is equal to `foreignField`
+  // If `justOne` is true, 'members' will be a single doc as opposed to
+  // an array. `justOne` is false by default.
+  justOne: false
+});
 
 module.exports = mongoose.model('post', postSchema)
